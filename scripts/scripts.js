fb65d990d488348532dcc88488d63f097b7ed735
Timilehin Daramola <timi.homezone111@gmail.com>
2024-11-02 00:42:50 -0400

    wdd231

---
+++ scripts/scripts.js
@@ -0,0 +1,31 @@
+const year = document.querySelector("#currentyear");
+let lastModified = document.querySelector("#lastModified");
+
+const today = new Date();
+
+year.innerHTML = `<span>${today.getFullYear()}</span>`;
+
+lastModified.innerHTML = `<span class="highlight">Last Modification: ${new Intl.DateTimeFormat(
+	"en-US",
+	{
+		dateStyle: "short"
+	}
+).format(today)}</span>`;
+
+
+// Store the selected elements that we are going to use. 
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
+
+/* ❔What does toggle mean?
+We could write separate add and remove statements. Toggle adds the class if it does not currently exist or removes the class if it does exist. 
+The CSS class rules will handle the different views, layouts, and displays.
+🗝️ JavaScript only applies the class value or not.
+*/
+
