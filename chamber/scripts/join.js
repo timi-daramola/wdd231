4d4d0d6d7ebeea42122436d7db62f118e3eaaf6e
Timilehin Daramola <timi.homezone111@gmail.com>
2024-12-02 17:14:23 -0500

    wdd231

---
+++ chamber/scripts/join.js
@@ -0,0 +1,8 @@
+document.getElementById('timestamp').value = new Date().toISOString();
+function openModal(id) {
+    document.getElementById(id).style.display = "block";
+}
+
+function closeModal(id) {
+    document.getElementById(id).style.display = "none";
+}
