69024044da778f119450a43d02836c3339c97286
Timilehin Daramola <timi.homezone111@gmail.com>
2024-12-02 17:56:10 -0500

    wdd231

--- chamber/scripts/join.js
+++ chamber/scripts/join.js
@@ -6,3 +6,12 @@ function openModal(id) {
 function closeModal(id) {
     document.getElementById(id).style.display = "none";
 }
+
+const params = new URLSearchParams(window.location.search);
+document.getElementById('displayFirstName').textContent = params.get('first-name');
+document.getElementById('displayLastName').textContent = params.get('last-name');
+document.getElementById('displayEmail').textContent = params.get('email');
+document.getElementById('displayPhone').textContent = params.get('phone');
+document.getElementById('displayOrgName').textContent = params.get('business-name');
+document.getElementById('displayMembershipLevel').textContent = params.get('membershipLevel');
+document.getElementById('displayTimestamp').textContent = params.get('timestamp');
